5b8ffba23b059fa305723baf232477412394822d
Didier Julien <Didier Julien>
2016-02-20 00:02:26 -0500

    initial commit

---
+++ serverinstall.sh
@@ -0,0 +1,13 @@
+#!sbin/bash
+
+echo "Please enter value for the following vhosts"
+echo "dev: "
+
+read dev
+
+echo "staging: "
+
+read staging
+
+echo "The dev environment is: $dev"
+echo "The staging environment is: $staging"
