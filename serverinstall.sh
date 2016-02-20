3bb1566f85be5008ca32f7bb10dc9f942759dc6b
Didier Julien <Didier Julien>
2016-02-20 00:37:56 -0500

    adding first case

--- serverinstall.sh
+++ serverinstall.sh
@@ -1,13 +1,26 @@
 #!sbin/bash
 
-echo "Please enter value for the following vhosts"
+echo "Hello! Please enter a value for the following vhosts"
 echo "dev: "
-
 read dev
 
 echo "staging: "
-
 read staging
 
 echo "The dev environment is: $dev"
 echo "The staging environment is: $staging"
+echo "Is this correct? (yes/no)"
+read answer
+
+case $answer in
+  y|yes|Y|Yes|YES) break ;;
+  *) echo "Please enter value for the following vhosts"
+  echo "dev: "
+  read dev
+
+  echo "staging: "
+  read staging
+
+  echo "The dev environment is: $dev"
+  echo "The staging environment is: $staging"
+esac
